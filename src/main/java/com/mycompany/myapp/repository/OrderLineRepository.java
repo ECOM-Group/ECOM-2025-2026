c6de8cf463931da4145143115100a2f047320fb0
Maxim Frolov <max.fro2021@gmail.com>
2025-10-24 13:12:31 +0200

    feat: added finding `OrderLine` for `ProdOrder`

--- src/main/java/com/mycompany/myapp/repository/OrderLineRepository.java
+++ src/main/java/com/mycompany/myapp/repository/OrderLineRepository.java
@@ -1,6 +1,7 @@
 package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.OrderLine;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {}
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+    List<OrderLine> findByProdOrderId(Long prodOrderId);
+}
