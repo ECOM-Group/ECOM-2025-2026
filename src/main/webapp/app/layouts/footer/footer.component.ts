1a44c4d3620368c7eb493f4502cc318afacbbff1
root <root@DESKTOP-BLPN861.>
2025-10-24 11:31:57 +0200

    feat(footer) Added a footer + fontaweseome

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { TranslateDirective } from 'app/shared/language';
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
   imports: [TranslateDirective],
 })
 export default class FooterComponent {}
