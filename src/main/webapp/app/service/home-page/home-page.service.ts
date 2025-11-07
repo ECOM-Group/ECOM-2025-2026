8ff69b2377bb95ab9d1874e883e3df90b4e2fea4
Hugo Poirot <poirothugo38@gmail.com>
2025-11-07 15:47:39 +0100

    fix(home-page) : correction apres pull request

--- src/main/webapp/app/service/home-page/home-page.service.ts
+++ src/main/webapp/app/service/home-page/home-page.service.ts
@@ -21,8 +21,8 @@ export class HomePageService {
     );
   }
 
-  GetConnnectedProducts(motsCles: String[]): Observable<IProduct[]> {
-    const params = new HttpParams().set('motsCles', motsCles.join(' '));
+  Search(motsCles: string[]): Observable<IProduct[]> {
+    const params = new HttpParams().set('q', motsCles.join(' '));
 
     return this.http.get<IProduct[]>('/api/products/search', { params }).pipe(
       map((products: IProduct[]) => {
@@ -33,33 +33,4 @@ export class HomePageService {
       }),
     );
   }
-
-  /*
-  GetMostSelledProducts(): Observable<IProduct[]> {
-    return this.http.get<IOrderLine[]>('/api/order-lines').pipe(
-      map((orderlines: IOrderLine[]) => {
-        const salesMap = new Map<number, number>();
-
-        orderlines.forEach(line => {
-          if (line.product && line.quantity) {
-            const current = salesMap.get(line.product.id) || 0;
-            salesMap.set(line.product.id, current + line.quantity);
-          } else {
-            console.log('Attention la ligne de commade est null, dans GetMostSelledProducts');
-          }
-        });
-
-        const topProductIds = Array.from(salesMap.entries())
-          .sort((a, b) => b[1] - a[1])
-          .slice(0, 5)
-          .map(([productId]) => productId);
-        return topProductIds;
-      }),
-
-      switchMap((topProductIds: number[]) => {
-        const requests = topProductIds.map(id => this.http.get<IProduct>(`/api/products/${id}`));
-        return forkJoin(requests);
-      }),
-    );
-  }*/
 }
