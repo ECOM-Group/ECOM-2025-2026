2a8c4cbef0ffdf53f6cbb6621a44185e47248355
Maxim Frolov <max.fro2021@gmail.com>
2025-12-04 14:28:16 +0100

    feat: basic tags on home page

--- src/main/webapp/app/layouts/home-page/home-page.component.ts
+++ src/main/webapp/app/layouts/home-page/home-page.component.ts
@@ -1,28 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { IProduct } from 'app/entities/product/product.model';
-import { switchMap } from 'rxjs';
+import { ITag } from 'app/entities/tag/tag.model';
 import { HomePageService } from 'app/service/home-page/home-page.service';
 import { MiniFicheComponent } from '../mini-fiche/mini-fiche.component';
 import { CartService } from 'app/service/cart/cart.service';
+import { TagService } from 'app/entities/tag/service/tag.service';
+import { TagLabelComponent } from 'app/entities/tag/tag-label/tag-label.component';
 
 @Component({
   selector: 'jhi-home-page',
-  imports: [MiniFicheComponent],
+  imports: [MiniFicheComponent, TagLabelComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss',
+  styleUrls: ['./home-page.component.scss'],
 })
 export default class HomePageComponent {
   private cartService = inject(CartService);
   topSoldProducts: IProduct[] = [];
   searchedProducts: IProduct[] = [];
+  tags: ITag[] = [];
+  selectedTagIds: number[] = [];
+
   constructor(
     private http: HttpClient,
     private homePageService: HomePageService,
+    private tagService: TagService,
   ) {}
 
   ngOnInit(): void {
     this.cartService.notifyCartUpdated();
+
+    // Top sold products
     this.homePageService.GetMostSelledProducts().subscribe({
       next: (products: IProduct[]) => {
         this.topSoldProducts = products;
@@ -31,7 +39,16 @@ export default class HomePageComponent {
         console.error('Erreur lors de la récupération des produits les plus vendus', err);
       },
     });
+
+    // Fetch tags
+    this.tagService.findAll().subscribe({
+      next: res => {
+        this.tags = res.body ?? [];
+      },
+      error: err => console.error('Erreur lors de la récupération des tags', err),
+    });
   }
+
   research(motsCles: string[]): void {
     this.homePageService.Search(motsCles).subscribe({
       next: (products: IProduct[]) => {
@@ -41,5 +58,11 @@ export default class HomePageComponent {
         console.error('Erreur lors de la récupération des produits connectes', err);
       },
     });
+  }
+
+  onTagClicked(tagId: number): void {
+    this.selectedTagIds = [tagId];
+    const tagName = this.tags.find(t => t.id === tagId)?.name;
+    if (tagName) this.research([tagName]);
   }
 }
