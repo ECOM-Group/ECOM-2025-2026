0c98928cdab4abf43ba6b1cb28a9bf0e5784c3ec
CercleTour <clement.bergere.38@gmail.com>
2025-12-04 14:06:54 +0100

    Home page fix

--- src/main/webapp/app/layouts/home-page/home-page.component.ts
+++ src/main/webapp/app/layouts/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { IProduct } from 'app/entities/product/product.model';
-import { switchMap } from 'rxjs';
+import { EMPTY, switchMap } from 'rxjs';
 import { HomePageService } from 'app/service/home-page/home-page.service';
 import { MiniFicheComponent } from '../mini-fiche/mini-fiche.component';
 import { CartService } from 'app/service/cart/cart.service';
@@ -21,16 +21,35 @@ export default class HomePageComponent {
     private homePageService: HomePageService,
   ) {}
 
+  private MIN_DISPLAYED_PRODUCTS = 10;
+
   ngOnInit(): void {
     this.cartService.notifyCartUpdated();
-    this.homePageService.GetMostSelledProducts().subscribe({
-      next: (products: IProduct[]) => {
-        this.topSoldProducts = products;
-      },
-      error: (err: any) => {
-        console.error('Erreur lors de la récupération des produits les plus vendus', err);
-      },
-    });
+    this.homePageService
+      .GetMostSelledProducts()
+      .pipe(
+        switchMap((products: IProduct[]) => {
+          this.topSoldProducts = products;
+
+          return this.topSoldProducts.length > this.MIN_DISPLAYED_PRODUCTS ? EMPTY : this.http.get<IProduct[]>('/api/products');
+        }),
+      )
+      .subscribe({
+        next: products => {
+          if (!products) return;
+
+          const topSoldProductsId = this.topSoldProducts.map(p => p.id);
+
+          const additionalProducts = products
+            .filter(p => !topSoldProductsId.includes(p.id))
+            .slice(0, this.MIN_DISPLAYED_PRODUCTS - this.topSoldProducts.length);
+
+          this.topSoldProducts.push(...additionalProducts);
+        },
+        error: err => {
+          console.error('Erreur lors du chargement des produits', err);
+        },
+      });
   }
   research(motsCles: string[]): void {
     this.homePageService.Search(motsCles).subscribe({
