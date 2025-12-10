0b36833eb94d2d3f8167609475ba585e1a35433e
Maxim Frolov <max.fro2021@gmail.com>
2025-12-10 09:21:38 +0100

    fix: fixing resources

--- src/main/java/com/mycompany/myapp/repository/ProductTagRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ProductTagRepository.java
@@ -4,10 +4,14 @@ import com.mycompany.myapp.domain.Tag;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductTagRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductTagRepository.class);
 
     @PersistenceContext
     private EntityManager em;
@@ -29,14 +33,27 @@ public class ProductTagRepository {
     }
 
     public List<Tag> findTagsForProduct(Long productId) {
-        return em
-            .createQuery("SELECT t FROM Tag t WHERE t.id IN " + "(SELECT r.tag_id FROM rel_tag__id r WHERE r.id_id = :pId)", Tag.class)
-            .setParameter("pId", productId)
-            .getResultList();
+        try {
+            @SuppressWarnings("unchecked")
+            List<Tag> result = em
+                .createNativeQuery("SELECT t.* FROM tag t " + "JOIN rel_tag__id r ON r.tag_id = t.id " + "WHERE r.id_id = :pId", Tag.class)
+                .setParameter("pId", productId)
+                .getResultList();
+
+            return result;
+        } catch (Exception e) {
+            LOG.error("Error while retrieving tags for product id={}", productId, e);
+            throw e;
+        }
     }
 
     @SuppressWarnings("unchecked")
     public List<Long> findProductIdsForTag(Long tagId) {
-        return em.createNativeQuery("SELECT id_id FROM rel_tag__id WHERE tag_id = :tId").setParameter("tId", tagId).getResultList();
+        try {
+            return em.createNativeQuery("SELECT id_id FROM rel_tag__id WHERE tag_id = :tId").setParameter("tId", tagId).getResultList();
+        } catch (Exception e) {
+            LOG.error("Error while products for tag_id id={}", tagId, e);
+            throw e;
+        }
     }
 }
