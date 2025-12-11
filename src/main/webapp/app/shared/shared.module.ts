80fa07e8d7c662acaf7aecad3ed42211462b9e1a
root <root@DESKTOP-BLPN861.>
2025-12-11 10:03:04 +0100

    feat(shared) Correctly Added the Icons

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fa-credit-card, fa-clock-rotate-left, fa-cart-shiopping, fa-linkedin, fa-instagram, fa-twitter, fa-facebook-f, fa-reply } from '@fortawesome/free-solid-svg-icons';
+import { faCreditCard, faClockRotateLeft, faCartShopping, faReply } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faInstagram, faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 
 import FindLanguageFromKeyPipe from './language/find-language-from-key.pipe';
